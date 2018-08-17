7b807ed302150fcad1f701f744aca6456884c04b
jason <libo529@gmail.com>
2018-08-17 14:48:07 +0800

    change default

--- src/js/index.js
+++ src/js/index.js
@@ -49,7 +49,68 @@ const FCE = function(options) {
   });
   initCy.call(self, {
     container: allElements["cy"],
-    rightMenus: options.rightMenus || []
+    rightMenus: options.rightMenus || [],
+    boxSelectionEnabled: false,
+  autounselectify: true,
+
+  style: [
+    {
+      selector: 'node',
+      css: {
+        'content': 'data(id)',
+        'text-valign': 'center',
+        'text-halign': 'center'
+      }
+    },
+    {
+      selector: '$node > node',
+      css: {
+        'padding-top': '10px',
+        'padding-left': '10px',
+        'padding-bottom': '10px',
+        'padding-right': '10px',
+        'text-valign': 'top',
+        'text-halign': 'center',
+        'background-color': '#bbb'
+      }
+    },
+    {
+      selector: 'edge',
+      css: {
+        'target-arrow-shape': 'triangle'
+      }
+    },
+    {
+      selector: ':selected',
+      css: {
+        'background-color': 'black',
+        'line-color': 'black',
+        'target-arrow-color': 'black',
+        'source-arrow-color': 'black'
+      }
+    }
+  ],
+
+  elements: {
+    nodes: [
+      { data: { id: 'a', parent: 'b' }, position: { x: 215, y: 85 } },
+      { data: { id: 'b' } },
+      { data: { id: 'c', parent: 'b' }, position: { x: 300, y: 85 } },
+      { data: { id: 'd' }, position: { x: 215, y: 175 } },
+      { data: { id: 'e' } },
+      { data: { id: 'f', parent: 'e' }, position: { x: 300, y: 175 } }
+    ],
+    edges: [
+      { data: { id: 'ad', source: 'a', target: 'd' } },
+      { data: { id: 'eb', source: 'e', target: 'b' } }
+
+    ]
+  },
+
+  layout: {
+    name: 'preset',
+    padding: 5
+  }
   });
   navbarsListener.call(self, _navbars);
   initZoomListener.call(self, zoom);
@@ -64,6 +125,7 @@ const FCE = function(options) {
 //   FCE.prototype[key] = item;
 // });
 Object.assign(FCE.prototype, Listener, {
+
   /**
    * 根据id查找toolbar对象
    * @param {String} id id
